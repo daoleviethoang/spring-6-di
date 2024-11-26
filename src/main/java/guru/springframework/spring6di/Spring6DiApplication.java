4d2b430e416450886fc4634a6b287688ba80c1c5
daoleviethoang <daoleviethoang@gmail.com>
2024-11-26 14:22:45 +0700

    delete unused libraries

--- src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -1,9 +1,7 @@
 package guru.springframework.spring6di;
 
-import guru.springframework.spring6di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
